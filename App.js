5977bd1a0abe9053f75b090321f078cee2efe47b
sunilhv2830 <sunilnaidu5555@gmail.com>
2020-05-15 11:04:52 +0530

    2 files modified

--- App.js
+++ App.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Conditional_Asnmt from './Conditional_Asnmt';
 import StateAsnmt from './StateAsnmt';
+import firstPhoto from 'images/user1.png';
+import secondPhoto from 'images/user2.png';
+import thirdPhoto from 'images/user3.png';
 import Footer from './Footer';
 
 
 
 class App extends Component {
- 
+ constructor(){
+    super();
+    this.state={
+      userdata:[{name:"SUNIL HV",dob:"28-02-1995",email:"sunil@gmail.com",firstPhoto},
+                {name:"ANIL HV",dob:"20-04-2000",email:"anil@gmail.com",secondPhoto},
+                {name:"PRASANNA SAGI",dob:"13-10-1777",email:"prasanna@gmail.com",thirdPhoto}      
+    ]
+    
+    }
+  }
+
   render() {
     return (
       <div>
@@ -15,6 +28,12 @@ class App extends Component {
        <Conditional_Asnmt></Conditional_Asnmt>
        <hr></hr>
        <StateAsnmt></StateAsnmt>
+       <h4 align="center">USER LIST</h4>
+      {
+      this.state.userdata.map((data)=>{
+        return  <SingleUser user={data}></SingleUser>
+        })
+      };
        <Footer></Footer>
       </div>
     );
